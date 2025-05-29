91350439a14e342361b848436e299d3653bd6f0e
Adi <159578677+yuurahz@users.noreply.github.com>
2025-05-30 01:47:55 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  apps: [
-    {
-      name: "yoshida",
-      script: "./index.js",
-      node_args: "--max-old-space-size=2048",
-      env: {
-        NODE_ENV: "production",
-      },
-      env_development: {
-        NODE_ENV: "development",
-      },
+  apps: [{
+    name: "yoshida",
+    script: "./index.js",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "2G",
+    node_args: "--max-old-space-size=2048",
+    env: {
+      NODE_ENV: "production"
     },
-  ],
+    env_development: {
+      NODE_ENV: "development"
+    }
+  }]
 };
